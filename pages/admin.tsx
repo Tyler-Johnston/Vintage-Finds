b68914c38d6716fb83d09e52594cfec17b32f00f
Tyler Johnston <johnstontyler@protonmail.com>
2023-04-19 10:22:57 -0600

    added header

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 
-export default function MyNewPage() {
+export default function Admin() {
   const [count, setCount] = useState<number>(0);
 
   return (
     <div>
-      <h1>Welcome to my new page!</h1>
+      <h1>Admin Page</h1>
       <input value="press me" type="button" onClick={() => setCount(count + 1)} />
       <p>This is some sample content for the page.</p>
       {count}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { HeaderSearch } from '../components/HeaderSearch/HeaderSearch';
 
 export default function HomePage() {
+  const headerLinks = [
+    { link: 'https://www.google.com', label: 'google' },
+    { link: 'https://duckduckgo.com', label: 'duckduckgo' },
+    { link: 'http://localhost:3000/hello', label: 'hello' },
+  ];
+
   return (
     <>
+      <HeaderSearch links={headerLinks} />
       <Welcome />
       <ColorSchemeToggle />
     </>
