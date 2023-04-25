8b1e3224763567605fffb626ab52529266a8e4b9
Tyler Johnston <johnstontyler@protonmail.com>
2023-04-24 22:01:59 -0600

    added user context

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,28 @@
 // import { ChangeEvent } from 'react';
 // import { ref, uploadBytes } from 'firebase/storage';
+import { signOut, onAuthStateChanged } from 'firebase/auth';
+import { useState, useEffect } from 'react';
+import UserContext, { User } from '../context/user';
 import { HeaderSearch } from '../components/HeaderSearch/HeaderSearch';
+import { auth } from '../lib/firebase';
+
 // import { storage } from '../lib/firebase';
 // import { GridDashboard } from '../components/Dashboard/GridDashboard';
 
 export default function HomePage() {
   // const [url, setUrl] = useState('');
+  const [user, setUser] = useState<User | null>(null);
   const headerLinks = [
     { link: 'https://www.facebook.com/people/Vintage-Finds-Utah/100030320875882/', label: 'facebook' },
   ];
+
+  useEffect(() => {
+    const cleanup = onAuthStateChanged(auth, (guest) => {
+      console.log('test: ', guest);
+      setUser(guest as User);
+    });
+    return cleanup;
+  }, []);
 
   // async function uploadFile(event: ChangeEvent<HTMLInputElement>) {
   //   const target = event.target as HTMLInputElement;
@@ -19,14 +33,15 @@ export default function HomePage() {
   // }
 
   return (
-    <>
+    <UserContext.Provider value={user}>
       <HeaderSearch links={headerLinks} />
-      {/* <h1>vintage finds</h1>
-      <input type="file" onChange={uploadFile} /> */}
+      <h1>vintage finds</h1>
+      <button type="button" onClick={() => signOut(auth)}>Sign out</button>
+      {/* <input type="file" onChange={uploadFile} /> */}
       {/* {url && (
         <img src={url} alt='my image' />
       )} */}
       {/* <GridDashboard /> */}
-    </>
+    </UserContext.Provider>
   );
 }
