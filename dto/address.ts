d004c9a7de8db3b2b7277a143df7874006330ed8
Tyler Johnston <johnstontyler@protonmail.com>
2023-04-27 01:03:44 -0600

    now displaying cheapest/bestvalue cost of shipping from your geolocation

---
+++ dto/address.ts
@@ -0,0 +1,10 @@
+export interface Address {
+    house_number: string;
+    road: string;
+    city: string;
+    state: string;
+    'ISO3166-2-lvl4'?: string;
+    postcode: string;
+    country: string;
+    country_code: string;
+}
