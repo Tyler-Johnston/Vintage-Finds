1ef70ea50fca9621764d37ca17f60e6c61292d64
Tyler Johnston <johnstontyler@protonmail.com>
2023-04-26 18:31:51 -0600

    can now upload images to the firebase storage

--- components/Data/CreateAntique.tsx
+++ components/Data/CreateAntique.tsx
@@ -1,21 +1,32 @@
 import { useState } from 'react';
-import { ref, set, push } from 'firebase/database';
+import { ref as databaseRef, set, push } from 'firebase/database';
+import { ref as storageRef, uploadBytes, getDownloadURL } from 'firebase/storage';
 import {
     TextInput,
-    // FileInput,
+    FileInput,
     Textarea,
     NumberInput,
     Button,
   } from '@mantine/core';
-import { db } from '../../lib/firebase';
+import { db, storage } from '../../lib/firebase';
 
 export default function CreateAntique() {
     const [name, setName] = useState('');
-    // const [picture, setPicture] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
     const [description, setDescription] = useState('');
     const [condition, setCondition] = useState('');
     const [price, setPrice] = useState(20);
     const [error, setError] = useState('');
+    const [image, setImage] = useState<File | null>(null);
+
+    async function uploadImage(id: string) {
+      if (image) {
+        const fileRef = storageRef(storage, `antiqueimages/${id}`);
+        await uploadBytes(fileRef, image);
+        const downloadUrl = await getDownloadURL(storageRef(storage, `antiqueimages/${id}`));
+        setImageUrl(downloadUrl);
+      }
+    }
 
     function checkInputs() {
         if (name === '' || condition === '' || description === '' || Number.isNaN(price)) {
@@ -36,14 +47,15 @@ export default function CreateAntique() {
       }
 
     async function writeUserData() {
-        const antiquesRef = ref(db, 'antiques/');
+        const antiquesRef = databaseRef(db, 'antiques/');
         const newAntiqueRef = push(antiquesRef);
         const newAntiqueKey = newAntiqueRef.key;
+        uploadImage(newAntiqueKey!);
 
         await set(newAntiqueRef, {
           id: newAntiqueKey,
           name,
-          // picture,
+          imageUrl,
           description,
           condition,
           price,
@@ -62,11 +74,14 @@ export default function CreateAntique() {
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
-            {/* <FileInput
-                placeholder="Pick file"
-                label="Image"
-                withAsterisk
-            /> */}
+          <FileInput
+            placeholder="Pick file"
+            label="Image"
+            accept="image/*"
+            withAsterisk
+            value={image}
+            onChange={(file: File | null) => setImage(file)}
+          />
           <Textarea
             placeholder="Buy this amazing product"
             label="Description"
